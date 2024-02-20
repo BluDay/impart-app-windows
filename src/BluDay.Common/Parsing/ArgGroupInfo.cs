8a2de0d9684083f62d35f304b6ce4d8136580e3d
BluDay <19403893+BluDay@users.noreply.github.com>
2024-02-20 21:17:03 +0100

    chore: remove optional parameters and create constructor overloads

--- src/BluDay.Common/Parsing/ArgGroupInfo.cs
+++ src/BluDay.Common/Parsing/ArgGroupInfo.cs
@@ -4,7 +4,7 @@ public sealed class ArgGroupInfo
 {
     public ArgInfo MainIdentifier => Identifiers[0];
 
-    public ArgActionType ActionType { get; }
+    public ArgActionType ActionType { get; } = ArgActionType.ParseValueByIdentifier;
 
     public object? Constant { get; }
 
@@ -12,17 +12,72 @@ public sealed class ArgGroupInfo
 
     public string? Description { get; }
 
-    public Type ValueType { get; }
+    public Type ValueType { get; } = typeof(bool);
 
     public IReadOnlyList<ArgInfo> Identifiers { get; }
 
+    public ArgGroupInfo(string name, string[] identifiers)
+        : this(
+           name:        name,
+           identifiers: identifiers,
+           description: null,
+           actionType:  ArgActionType.ParseValueByIdentifier,
+           valueType:   null,
+           constant:    null
+        )
+    { }
+
+    public ArgGroupInfo(string name, string[] identifiers, string? description)
+        : this(
+            name:        name,
+            identifiers: identifiers,
+            description: description,
+            actionType:  ArgActionType.ParseValueByIdentifier,
+            valueType:   null,
+            constant:    null
+        )
+    { }
+
     public ArgGroupInfo(
-        string          name,
-        string[]        identifiers,
-        string?         description = null,
-        ArgActionType   actionType  = ArgActionType.ParseValueByIdentifier,
-        Type?           valueType   = null,
-        object?         constant    = null)
+        string        name,
+        string[]      identifiers,
+        string?       description,
+        ArgActionType actionType
+    )
+        : this(
+            name:        name,
+            identifiers: identifiers,
+            description: description,
+            actionType:  actionType,
+            valueType:   null,
+            constant:    null
+        )
+    { }
+
+    public ArgGroupInfo(
+        string        name,
+        string[]      identifiers,
+        string?       description,
+        ArgActionType actionType,
+        Type?         valueType
+    )
+        : this(
+            name:        name,
+            identifiers: identifiers,
+            description: description,
+            actionType:  actionType,
+            valueType:   valueType,
+            constant:    null
+        )
+    { }
+
+    public ArgGroupInfo(
+        string        name,
+        string[]      identifiers,
+        string?       description,
+        ArgActionType actionType,
+        Type?         valueType,
+        object?       constant)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(name);
 
