54dbbaf86464b0f817f568876a7844a98704f03c
BluDay <19403893+BluDay@users.noreply.github.com>
2024-02-21 22:30:33 +0100

    chore: update infrastructure usings

--- src/BluDay.Impart.App/Usings.cs
+++ src/BluDay.Impart.App/Usings.cs
@@ -1,11 +1,10 @@
-﻿global using BluDay.Common;
-global using BluDay.Common.Extensions;
+﻿global using BluDay.Common.Extensions;
 global using BluDay.Common.Extensions.DependencyInjection;
-global using BluDay.Common.Infrastructure.Models;
-global using BluDay.Common.Infrastructure.ViewModels;
+global using BluDay.Common.Models;
+global using BluDay.Common.ViewModels;
 global using BluDay.Common.CommandLine;
 global using BluDay.Common.Services;
-global using BluDay.Common.UI;
+global using BluDay.Common.UI.Appearance;
 global using BluDay.Common.UI.Navigation;
 global using BluDay.Common.UI.WindowManagement;
 global using BluDay.Impart.App.Domain.Models;
