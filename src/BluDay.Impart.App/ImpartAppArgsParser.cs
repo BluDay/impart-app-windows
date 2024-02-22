f1399f850ae92adbab51cf19578b3f3859782f1e
BluDay <19403893+BluDay@users.noreply.github.com>
2024-02-22 20:35:56 +0100

    chore: try new enumeration code style in CreateArgs

--- src/BluDay.Impart.App/ImpartAppArgsParser.cs
+++ src/BluDay.Impart.App/ImpartAppArgsParser.cs
@@ -6,40 +6,41 @@ public sealed class ImpartAppArgsParser : ArgsParser<ImpartAppArgs>
 
     public ImpartAppArgsParser() : base(args: CreateArgs()) { }
 
-    private static IEnumerable<IArgInfo> CreateArgs()
+    private static IEnumerable<ArgInfo> CreateArgs()
     {
-        return new ArgInfo[]
+        yield return new("-d", "--demo-mode")
         {
-            new("-d", "--demo-mode")
-            {
-                Name        = nameof(ImpartAppArgs.DemoMode),
-                Description = "Launch the app in demo mode."
-            },
-            new("-b", "--peformance-mode")
-            {
-                Name        = nameof(ImpartAppArgs.PerformanceMode),
-                Description = "Launch the app in performance mode."
-            },
-            new("--skip-intro")
-            {
-                Name        = nameof(ImpartAppArgs.SkipIntro),
-                Description = "Skip the first-time launch introduction."
-            },
-            new("-v", "--verbosity")
-            {
-                Name        = nameof(ImpartAppArgs.Verbosity),
-                Description = "Verbosity level.",
-                ActionType  = ArgActionType.AddConstant,
-                ValueType   = typeof(uint),
-                Constant    = (uint)1
-            },
-            new("-t", "--theme")
-            {
-                Name        = nameof(ImpartAppArgs.AppTheme),
-                Description = "App theme to use at launch.",
-                ActionType  = ArgActionType.ParseValue,
-                ValueType   = typeof(string)
-            }
+            Name        = nameof(ImpartAppArgs.DemoMode),
+            Description = "Launch the app in demo mode."
+        };
+
+        yield return new("-b", "--peformance-mode")
+        {
+            Name        = nameof(ImpartAppArgs.PerformanceMode),
+            Description = "Launch the app in performance mode."
+        };
+
+        yield return new("--skip-intro")
+        {
+            Name        = nameof(ImpartAppArgs.SkipIntro),
+            Description = "Skip the first-time launch introduction."
+        };
+
+        yield return new("-v", "--verbosity")
+        {
+            Name        = nameof(ImpartAppArgs.Verbosity),
+            Description = "Verbosity level.",
+            ActionType  = ArgActionType.AddConstant,
+            ValueType   = typeof(uint),
+            Constant    = (uint)1
+        };
+
+        yield return new("-t", "--theme")
+        {
+            Name        = nameof(ImpartAppArgs.AppTheme),
+            Description = "App theme to use at launch.",
+            ActionType  = ArgActionType.ParseValue,
+            ValueType   = typeof(string)
         };
     }
 }
