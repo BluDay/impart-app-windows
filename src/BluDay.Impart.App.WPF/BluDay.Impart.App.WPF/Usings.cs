766fc51002a7716def82d199d43b532fa37fec00
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-03 22:18:56 +0100

    feat: add BluDay.Net assembly reference

--- src/BluDay.Impart.App.WPF/BluDay.Impart.App.WPF/Usings.cs
+++ src/BluDay.Impart.App.WPF/BluDay.Impart.App.WPF/Usings.cs
@@ -1,5 +1,3 @@
-﻿global using BluDay.Impart.App.WPF.UI.Controls;
-global using BluDay.Net.Attributes;
+﻿global using BluDay.Net.Attributes;
 global using Microsoft.Extensions.DependencyInjection;
-global using System.Diagnostics;
 global using System.Windows;
--- src/BluDay.Impart.App.WinUI/BluDay.Impart.App.WinUI/Usings.cs
+++ src/BluDay.Impart.App.WinUI/BluDay.Impart.App.WinUI/Usings.cs
@@ -11,4 +11,3 @@ global using Microsoft.UI.Xaml.Media;
 global using Microsoft.UI.Xaml.Media.Animation;
 global using System;
 global using System.Collections.Generic;
-global using System.Linq;
