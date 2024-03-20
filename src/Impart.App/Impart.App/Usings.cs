028d22ba5676e7dbf56ecbf377879f0984c8d6bd
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-20 17:07:33 +0100

    feat: correct usings

--- src/Impart.App/Impart.App/Usings.cs
+++ src/Impart.App/Impart.App/Usings.cs
@@ -1,7 +1,7 @@
 ﻿global using BluDay.Net.Common.CommandLine;
-global using BluDay.Net.Common.Extensions.DependencyInjection;
-global using BluDay.Net.Common.Models;
-global using BluDay.Net.Common.Services;
+global using BluDay.Net.DependencyInjection;
+global using BluDay.Net.Models;
+global using BluDay.Net.Services;
 global using CommunityToolkit.Mvvm.ComponentModel;
 global using CommunityToolkit.Mvvm.Messaging;
 global using Impart.App.Domain.Models;
