f79cb7118381d8cb4ef89cc29eb40953ee9341d9
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-21 13:37:49 +0100

    chore: update property names

--- src/Impart.App/Impart.App/ImpartAppArgsParser.cs
+++ src/Impart.App/Impart.App/ImpartAppArgsParser.cs
@@ -10,32 +10,32 @@ public sealed class ImpartAppArgsParser : ArgumentsParser<ImpartAppArgs>
     [
         new ArgumentInfo<bool>()
         {
-            Flag        = new("d|demo-mode"),
+            Flags       = new("d|demo-mode"),
             Name        = nameof(ImpartAppArgs.DemoMode),
             Description = "Launch the app in demo mode."
         },
         new ArgumentInfo<bool>()
         {
-            Flag        = new("b|performance-mode"),
+            Flags       = new("b|performance-mode"),
             Name        = nameof(ImpartAppArgs.PerformanceMode),
             Description = "Launch the app in performance mode."
         },
         new ArgumentInfo<bool>()
         {
-            Flag        = new("skip-intro"),
+            Flags       = new("skip-intro"),
             Name        = nameof(ImpartAppArgs.SkipIntro),
             Description = "Skip the firsttime launch introduction."
         },
         new ArgumentInfo<int>()
         {
-            Flag        = new("v|verbosity"),
+            Flags       = new("v|verbosity"),
             Name        = nameof(ImpartAppArgs.Verbosity),
             Description = "Verbosity level.",
             ActionType  = ArgumentActionType.Count
         },
         new ArgumentInfo<AppTheme>()
         {
-            Flag        = new("t|app-theme"),
+            Flags       = new("t|app-theme"),
             Name        = nameof(ImpartAppArgs.AppTheme),
             Description = "App theme to use at launch.",
             ActionType  = ArgumentActionType.StoreValue,
