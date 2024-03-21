634e13402311c388de310f5adc891c7db867a974
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-21 12:41:03 +0100

    feat: change method return type, make non-static and use collection expression

--- src/Impart.App/Impart.App/ImpartAppArgsParser.cs
+++ src/Impart.App/Impart.App/ImpartAppArgsParser.cs
@@ -6,39 +6,35 @@ public sealed class ImpartAppArgsParser : ArgumentsParser<ImpartAppArgs>
 
     public ImpartAppArgsParser() : base(CreateArguments()) { }
 
-    private static IEnumerable<ArgumentInfo> CreateArguments()
-    {
-        yield return new("d|demo-mode")
+    private ArgumentInfo[] CreateArguments() =>
+    [
+        new("d|demo-mode")
         {
             Name        = nameof(ImpartAppArgs.DemoMode),
             Description = "Launch the app in demo mode."
-        };
-
-        yield return new("b|performance-mode")
+        },
+        new("b|performance-mode")
         {
             Name        = nameof(ImpartAppArgs.PerformanceMode),
             Description = "Launch the app in performance mode."
-        };
-
-        yield return new("skip-intro")
+        },
+        new("skip-intro")
         {
             Name        = nameof(ImpartAppArgs.SkipIntro),
             Description = "Skip the firsttime launch introduction."
-        };
-
-        yield return new("v|verbosity")
+        },
+        new("v|verbosity")
         {
             Name        = nameof(ImpartAppArgs.Verbosity),
             Description = "Verbosity level.",
             ActionType  = ArgumentActionType.Count
-        };
-
-        yield return new("t|app-theme")
+        },
+        new("t|app-theme")
         {
             Name        = nameof(ImpartAppArgs.AppTheme),
             Description = "App theme to use at launch.",
             ActionType  = ArgumentActionType.StoreValue,
             StoreType   = ArgumentStoreType.String
-        };
-    }
+        }
+    ];
 }
