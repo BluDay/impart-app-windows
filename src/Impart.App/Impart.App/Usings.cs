a75bce3ee2bae0964b55862e45b3c7bca8b29d3a
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-21 14:32:03 +0100

    chore: correct usings

--- src/Impart.App/Impart.App/Usings.cs
+++ src/Impart.App/Impart.App/Usings.cs
@@ -1,8 +1,8 @@
 ﻿global using BluDay.Net.Common.Attributes;
 global using BluDay.Net.Common.CommandLine;
 global using BluDay.Net.Common.DependencyInjection;
+global using BluDay.Net.Common.Extensions;
 global using BluDay.Net.Domain.Models;
-global using BluDay.Net.Domain.ViewModels;
 global using BluDay.Net.Services;
 global using BluDay.Net.UI.Appearance;
 global using CommunityToolkit.Mvvm.ComponentModel;
