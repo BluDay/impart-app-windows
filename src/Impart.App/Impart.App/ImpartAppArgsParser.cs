68eb9b06c2aeff8d5f546183613120fc00a07e19
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-22 21:46:00 +0100

    chore: update argument types

--- src/Impart.App/Impart.App/ImpartAppArgsParser.cs
+++ src/Impart.App/Impart.App/ImpartAppArgsParser.cs
@@ -6,36 +6,36 @@ public sealed class ImpartAppArgsParser : ArgumentsParser<ImpartAppArgs>
 
     public ImpartAppArgsParser() : base(CreateArgs()) { }
 
-    private static IArgument[] CreateArgs() =>
+    private static Argument[] CreateArgs() =>
     [
-        new Argument<bool>("d|demo-mode")
+        new Argument("d|demo-mode")
         {
             Name        = nameof(ImpartAppArgs.DemoMode),
             Description = "Launch the app in demo mode."
         },
-        new Argument<bool>("b|performance-mode")
+        new Argument("b|performance-mode")
         {
             Name        = nameof(ImpartAppArgs.PerformanceMode),
             Description = "Launch the app in performance mode."
         },
-        new Argument<bool>("skip-intro")
+        new Argument("skip-intro")
         {
             Name        = nameof(ImpartAppArgs.SkipIntro),
             Description = "Skip the firsttime launch introduction."
         },
-        new Argument<uint>("v|verbosity")
+        new Argument("v|verbosity")
         {
             Name        = nameof(ImpartAppArgs.Verbosity),
             Description = "Verbosity level.",
             ActionType  = ArgumentActionType.CountFlag
         },
-        new Argument<AppTheme>("t|app-theme")
+        new Argument("t|app-theme")
         {
             Name         = nameof(ImpartAppArgs.AppTheme),
             Description  = "App theme to use at launch.",
             ActionType   = ArgumentActionType.StoreValue,
             StoreType    = ArgumentStoreType.String,
-            ValueHandler = Enum.Parse<AppTheme>
+            ValueHandler = arg => Enum.Parse<AppTheme>(arg)
         }
     ];
 }
