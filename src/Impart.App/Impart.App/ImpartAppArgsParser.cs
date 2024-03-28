41190a9f02de43dcde4873916f81959f8bbf62ac
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-28 15:02:10 +0100

    feat: change return type and add new postional argument descriptor

--- src/Impart.App/Impart.App/ImpartAppArgsParser.cs
+++ src/Impart.App/Impart.App/ImpartAppArgsParser.cs
@@ -6,37 +6,38 @@ public sealed class ImpartAppArgsParser : ArgumentsParser<ImpartAppArgs>
 
     public ImpartAppArgsParser() : base(CreateArgs()) { }
 
-    private static IReadOnlyList<OptionalArgumentDescriptor> CreateArgs() =>
+    private static IReadOnlyList<IArgumentDescriptor> CreateArgs() =>
     [
-        new(CommandLineArgumentFlagDescriptors.DEMO_MODE)
+        new OptionalArgumentDescriptor(CommandLineArgumentFlagDescriptors.DEMO_MODE)
         {
             Name        = nameof(ImpartAppArgs.DemoMode),
             Description = CommandLineArgumentDescriptions.DEMO_MODE
         },
-        new(CommandLineArgumentFlagDescriptors.PERFORMANCE_MODE)
+        new OptionalArgumentDescriptor(CommandLineArgumentFlagDescriptors.PERFORMANCE_MODE)
         {
             Name        = nameof(ImpartAppArgs.PerformanceMode),
             Description = CommandLineArgumentDescriptions.PERFORMANCE_MODE
         },
-        new(CommandLineArgumentFlagDescriptors.SKIP_INTRO)
+        new OptionalArgumentDescriptor(CommandLineArgumentFlagDescriptors.SKIP_INTRO)
         {
             Name        = nameof(ImpartAppArgs.SkipIntro),
             Description = CommandLineArgumentDescriptions.SKIP_INTRO
         },
-        new(CommandLineArgumentFlagDescriptors.VERBOSITY)
+        new OptionalArgumentDescriptor(CommandLineArgumentFlagDescriptors.VERBOSITY)
         {
             Name        = nameof(ImpartAppArgs.Verbosity),
             Description = CommandLineArgumentDescriptions.VERBOSITY,
             ActionType  = ArgumentActionType.CountFlag,
             StoreType   = ArgumentStoreType.Integer
         },
-        new(CommandLineArgumentFlagDescriptors.APP_THEME)
+        new OptionalArgumentDescriptor(CommandLineArgumentFlagDescriptors.APP_THEME)
         {
             Name        = nameof(ImpartAppArgs.AppTheme),
             Description = CommandLineArgumentDescriptions.APP_THEME,
             ActionType  = ArgumentActionType.StoreValue,
             StoreType   = ArgumentStoreType.String
-        }
+        },
+        new PositionalArgumentDescriptor()
     ];
 
     public static ImpartAppArgs ParseFromCommandLine()
