355783085592c110a8ee87e03cc74d77efc8e667
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-31 23:14:10 +0200

    chore: use a proper scope style

--- src/Impart.App/Impart.App/ImpartAppArgsParser.cs
+++ src/Impart.App/Impart.App/ImpartAppArgsParser.cs
@@ -4,38 +4,40 @@ public sealed class ImpartAppArgsParser : ArgumentsParser<ImpartAppArgs>
 {
     public ImpartAppArgsParser() : base(CreateOptionals(), CreatePositional()) { }
 
-    private static IEnumerable<OptionalArgument> CreateOptionals() =>
-    [
-        new(ArgumentFlagDescriptors.DEMO_MODE)
-        {
-            Name        = nameof(ImpartAppArgs.DemoMode),
-            Description = ArgumentDescriptions.DEMO_MODE
-        },
-        new(ArgumentFlagDescriptors.PERFORMANCE_MODE)
-        {
-            Name        = nameof(ImpartAppArgs.PerformanceMode),
-            Description = ArgumentDescriptions.PERFORMANCE_MODE
-        },
-        new(ArgumentFlagDescriptors.SKIP_INTRO)
-        {
-            Name        = nameof(ImpartAppArgs.SkipIntro),
-            Description = ArgumentDescriptions.SKIP_INTRO
-        },
-        new(ArgumentFlagDescriptors.VERBOSITY)
-        {
-            Name        = nameof(ImpartAppArgs.Verbosity),
-            Description = ArgumentDescriptions.VERBOSITY,
-            ActionType  = ArgumentActionType.CountFlag,
-            StoreType   = ArgumentStoreType.Integer
-        },
-        new(ArgumentFlagDescriptors.APP_THEME)
-        {
-            Name        = nameof(ImpartAppArgs.AppTheme),
-            Description = ArgumentDescriptions.APP_THEME,
-            ActionType  = ArgumentActionType.StoreValue,
-            StoreType   = ArgumentStoreType.String
-        }
-    ];
+    private static IEnumerable<OptionalArgument> CreateOptionals()
+    {
+        return [
+            new(ArgumentFlagDescriptors.DEMO_MODE)
+            {
+                Name        = nameof(ImpartAppArgs.DemoMode),
+                Description = ArgumentDescriptions.DEMO_MODE
+            },
+            new(ArgumentFlagDescriptors.PERFORMANCE_MODE)
+            {
+                Name        = nameof(ImpartAppArgs.PerformanceMode),
+                Description = ArgumentDescriptions.PERFORMANCE_MODE
+            },
+            new(ArgumentFlagDescriptors.SKIP_INTRO)
+            {
+                Name        = nameof(ImpartAppArgs.SkipIntro),
+                Description = ArgumentDescriptions.SKIP_INTRO
+            },
+            new(ArgumentFlagDescriptors.VERBOSITY)
+            {
+                Name        = nameof(ImpartAppArgs.Verbosity),
+                Description = ArgumentDescriptions.VERBOSITY,
+                ActionType  = ArgumentActionType.CountFlag,
+                StoreType   = ArgumentStoreType.Integer
+            },
+            new(ArgumentFlagDescriptors.APP_THEME)
+            {
+                Name        = nameof(ImpartAppArgs.AppTheme),
+                Description = ArgumentDescriptions.APP_THEME,
+                ActionType  = ArgumentActionType.StoreValue,
+                StoreType   = ArgumentStoreType.String
+            }
+        ];
+    }
 
     private static PositionalArgument CreatePositional()
     {
