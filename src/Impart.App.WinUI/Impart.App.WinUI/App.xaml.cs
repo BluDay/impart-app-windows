3a7739b3ae1879186624715bf28dfabe73df3bdc
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-02 18:52:34 +0200

    chore: update method name

--- src/Impart.App.WinUI/Impart.App.WinUI/App.xaml.cs
+++ src/Impart.App.WinUI/Impart.App.WinUI/App.xaml.cs
@@ -19,7 +19,7 @@ public sealed partial class App : Application
     /// <param name="e">Details about the launch request and process.</param>
     protected override void OnLaunched(LaunchActivatedEventArgs e)
     {
-        ImpartAppArguments arguments = new ImpartAppArgumentsParser().ParseFromCommandLine();
+        ImpartAppArguments arguments = new ImpartAppArgumentsParser().Parse();
 
         _app = new(arguments);
 
