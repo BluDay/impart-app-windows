d20ae9b59d343e27aef57344f56e9f99adf43af6
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-02 21:05:34 +0200

    chore: add xml comments for ImpartAppContainer class

--- src/Impart.App/Impart.App/Core/ImpartAppContainer.cs
+++ src/Impart.App/Impart.App/Core/ImpartAppContainer.cs
@@ -1,5 +1,9 @@
 namespace Impart.App.Core;
 
+/// <summary>
+/// Handles the management and isolation of application services, providing a cohesive
+/// container for initialization, resource handling, and efficient access to dependencies.
+/// </summary>
 internal sealed class ImpartAppContainer : IDisposable
 {
     private readonly ImpartApp _app;
@@ -8,13 +12,23 @@ internal sealed class ImpartAppContainer : IDisposable
 
     private readonly IServiceCollection _serviceDescriptors;
 
+    /// <summary>
+    /// Gets the service provider for resolving service instances.
+    /// </summary>
     public IServiceProvider ServiceProvider => _serviceProvider;
 
+    /// <summary>
+    /// Gets a read-only list of descriptors for all registered services.
+    /// </summary>
     public IReadOnlyList<ServiceDescriptor> ServiceDescriptors
     {
         get => _serviceDescriptors.AsReadOnly();
     }
 
+    /// <summary>
+    /// Initializes a new instance for the provided app instance.
+    /// </summary>
+    /// <param name="app">The app instance.</param>
     public ImpartAppContainer(ImpartApp app)
     {
         ArgumentNullException.ThrowIfNull(app);
@@ -28,6 +42,10 @@ internal sealed class ImpartAppContainer : IDisposable
         _serviceProvider = _serviceDescriptors.BuildServiceProvider();
     }
 
+    /// <summary>
+    /// Registers service descriptors.
+    /// </summary>
+    /// <returns>A service collection and container builder.</returns>
     private static IServiceCollection CreateServiceDescriptors()
     {
         return new ServiceCollection()
@@ -40,6 +58,10 @@ internal sealed class ImpartAppContainer : IDisposable
             .AddSingleton<IViewModelProvider, ViewModelProvider>();
     }
 
+    /// <summary>
+    /// Disposes of all service instances and the container itself.
+    /// </summary>
+    /// <exception cref="NotImplementedException"></exception>
     public void Dispose()
     {
         throw new NotImplementedException();
