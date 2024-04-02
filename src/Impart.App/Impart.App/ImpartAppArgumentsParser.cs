dcf879128c0ca0801825ff89cf62e75de5de7f06
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-02 15:43:11 +0200

    feat: add XML comments to args-parser class and use old yield return method

--- src/Impart.App/Impart.App/ImpartAppArgumentsParser.cs
+++ src/Impart.App/Impart.App/ImpartAppArgumentsParser.cs
@@ -1,46 +1,61 @@
 namespace Impart.App;
 
+/// <summary>
+/// An <see cref="ArgumentsParser{TArguments}"/> class for parsing command-line arguments to an <see cref="ImpartAppArguments"/> instance.
+/// </summary>
 public sealed class ImpartAppArgumentsParser : ArgumentsParser<ImpartAppArguments>
 {
+    /// <summary>
+    /// Initializes a new instance with optional and positional arguments for <see cref="ImpartApp"/>.
+    /// </summary>
     public ImpartAppArgumentsParser() : base(CreateOptionals(), CreatePositional()) { }
 
+    /// <summary>
+    /// Creates optional argument descriptors to register.
+    /// </summary>
+    /// <returns>An enumerable of optional arguments.</returns>
     private static IEnumerable<OptionalArgument> CreateOptionals()
     {
-        return new List<OptionalArgument>()
+        yield return new(ArgumentFlagDescriptors.DEMO_MODE)
         {
-            new(ArgumentFlagDescriptors.DEMO_MODE)
-            {
-                Name        = nameof(ImpartAppArguments.DemoMode),
-                Description = ArgumentDescriptions.DEMO_MODE
-            },
-            new(ArgumentFlagDescriptors.PERFORMANCE_MODE)
-            {
-                Name        = nameof(ImpartAppArguments.PerformanceMode),
-                Description = ArgumentDescriptions.PERFORMANCE_MODE
-            },
-            new(ArgumentFlagDescriptors.SKIP_INTRO)
-            {
-                Name        = nameof(ImpartAppArguments.SkipIntro),
-                Description = ArgumentDescriptions.SKIP_INTRO
-            },
-            new(ArgumentFlagDescriptors.VERBOSITY)
-            {
-                Name        = nameof(ImpartAppArguments.Verbosity),
-                Description = ArgumentDescriptions.VERBOSITY,
-                ActionKind  = ArgumentActionKind.CountFlag,
-                StoreKind   = ArgumentStoreKind.Integer,
-                Constant    = 1
-            },
-            new(ArgumentFlagDescriptors.APP_THEME)
-            {
-                Name        = nameof(ImpartAppArguments.AppTheme),
-                Description = ArgumentDescriptions.APP_THEME,
-                ActionKind  = ArgumentActionKind.StoreValue,
-                StoreKind   = ArgumentStoreKind.String
-            }
+            Name        = nameof(ImpartAppArguments.DemoMode),
+            Description = ArgumentDescriptions.DEMO_MODE
+        };
+
+        yield return new(ArgumentFlagDescriptors.PERFORMANCE_MODE)
+        {
+            Name        = nameof(ImpartAppArguments.PerformanceMode),
+            Description = ArgumentDescriptions.PERFORMANCE_MODE
+        };
+
+        yield return new(ArgumentFlagDescriptors.SKIP_INTRO)
+        {
+            Name        = nameof(ImpartAppArguments.SkipIntro),
+            Description = ArgumentDescriptions.SKIP_INTRO
+        };
+
+        yield return new(ArgumentFlagDescriptors.VERBOSITY)
+        {
+            Name        = nameof(ImpartAppArguments.Verbosity),
+            Description = ArgumentDescriptions.VERBOSITY,
+            ActionKind  = ArgumentActionKind.CountFlag,
+            StoreKind   = ArgumentStoreKind.Integer,
+            Constant    = 1
+        };
+
+        yield return new(ArgumentFlagDescriptors.APP_THEME)
+        {
+            Name        = nameof(ImpartAppArguments.AppTheme),
+            Description = ArgumentDescriptions.APP_THEME,
+            ActionKind  = ArgumentActionKind.StoreValue,
+            StoreKind   = ArgumentStoreKind.String
         };
     }
 
+    /// <summary>
+    /// Creates a positional argument descriptor to register.
+    /// </summary>
+    /// <returns>A positional argument instance.</returns>
     private static PositionalArgument CreatePositional()
     {
         return new()
