cd113fb55dd1f507e6eee2f197d460b0a2118175
BluDay <19403893+BluDay@users.noreply.github.com>
2024-06-20 11:34:05 +0200

    feat: correct namespace in App for WPF

--- src/BluDay.Impart.WPF/BluDay.Impart.WPF/App.xaml.cs
+++ src/BluDay.Impart.WPF/BluDay.Impart.WPF/App.xaml.cs
@@ -5,15 +5,22 @@
 /// </summary>
 public sealed partial class App : Application
 {
+    private Shell? _mainWindow;
+
     private readonly ImpartApp _app;
+
+    private readonly Func<Shell> _windowFactory;
 
     /// <summary>
     /// Initializes the application object.
     /// </summary>
     /// <param name="app">The app instance for Impart.</param>
-    public App(ImpartApp app)
+    /// <param name="windowFactory">The window factory.</param>
+    public App(ImpartApp app, Func<Shell> windowFactory)
     {
         _app = app;
+
+        _windowFactory = windowFactory;
     }
 
     /// <summary>
@@ -23,6 +30,11 @@ public sealed partial class App : Application
     protected override void OnStartup(StartupEventArgs e)
     {
         _app.Initialize();
+
+        _mainWindow = _windowFactory.Invoke();
+
+        _mainWindow.Activate();
+        _mainWindow.Show();
 
         base.OnStartup(e);
     }
