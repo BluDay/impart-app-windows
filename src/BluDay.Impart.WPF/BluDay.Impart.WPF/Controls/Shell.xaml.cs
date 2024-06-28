1927781162e739cf110d3c02ad65a3d46caf04d7
BluDay <19403893+BluDay@users.noreply.github.com>
2024-06-28 13:48:41 +0200

    feat(wpf): implement INavigableWindow for the Shell class

--- src/BluDay.Impart.WPF/BluDay.Impart.WPF/Controls/Shell.xaml.cs
+++ src/BluDay.Impart.WPF/BluDay.Impart.WPF/Controls/Shell.xaml.cs
@@ -3,7 +3,32 @@
 /// <summary>
 /// Interaction logic for Shell.xaml
 /// </summary>
-public sealed partial class Shell : Window
+public sealed partial class Shell : Window, INavigableWindow
 {
-    public Shell() => InitializeComponent();
+    private readonly IViewNavigator _viewNavigator;
+
+    public IViewNavigator ViewNavigator => _viewNavigator;
+
+    public Guid Id { get; }
+
+    public Shell()
+    {
+        _viewNavigator = null!;
+
+        Id = Guid.NewGuid();
+
+        InitializeComponent();
+    }
+
+    void IWindow.Activate()
+    {
+        Activate();
+        Show();
+    }
+
+    public void Resize(int width, int height)
+    {
+        Width  = width;
+        Height = height;
+    }
 }
--- src/BluDay.Impart.WPF/BluDay.Impart.WPF/Usings.cs
+++ src/BluDay.Impart.WPF/BluDay.Impart.WPF/Usings.cs
@@ -5,6 +5,8 @@ global using BluDay.Impart.Domain.ViewModels;
 global using BluDay.Impart.WPF;
 global using BluDay.Impart.WPF.Controls;
 global using BluDay.Impart.WPF.Views;
+global using BluDay.Net.UI.Navigation;
+global using BluDay.Net.UI.WindowManagement;
 global using BluDay.Net.Services;
 
 global using Microsoft.Extensions.DependencyInjection;
