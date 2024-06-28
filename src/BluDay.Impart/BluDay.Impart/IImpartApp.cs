bfccea7ea4e47056ab139ef72233daac28d25998
BluDay <19403893+BluDay@users.noreply.github.com>
2024-06-28 14:18:52 +0200

    feat: re-introduce interfaces for ImpartApp and ImpartAppArgs

---
+++ src/BluDay.Impart/BluDay.Impart/IImpartApp.cs
@@ -0,0 +1,33 @@
+﻿namespace BluDay.Impart;
+
+/// <summary>
+/// The application core for Impart.
+/// </summary>
+public interface IImpartApp
+{
+    /// <summary>
+    /// Gets instance of parsed command-line arguments.
+    /// </summary>
+    IImpartAppArgs Args { get; }
+
+    /// <summary>
+    /// Gets a value indicating whether the app has been disposed.
+    /// </summary>
+    bool IsDisposed { get; }
+
+    /// <summary>
+    /// Gets a value indicating whether the app has been initialized.
+    /// </summary>
+    bool IsInitialized { get; }
+
+    /// <summary>
+    /// Stops the application and disposed of all services and the DI container.
+    /// </summary>
+    void Dispose();
+
+    /// <summary>
+    /// Initializes the entire application.
+    /// </summary>
+    /// <exception cref="ObjectDisposedException">If the instance has been disposed.</exception>
+    void Initialize();
+}
--- src/BluDay.Impart/BluDay.Impart/IImpartAppArgs.cs
+++ src/BluDay.Impart/BluDay.Impart/IImpartAppArgs.cs
@@ -3,35 +3,30 @@ namespace BluDay.Impart;
 /// <summary>
 /// Represents parsed command-line arguments. Parsed arguments will have non-null values.
 /// </summary>
-public sealed class ImpartAppArgs : IImpartAppArgs
+public interface IImpartAppArgs
 {
     /// <summary>
     /// Gets a value indicating whether the app be run in demo mode.
     /// </summary>
-    [OptionalArgument]
-    public bool? DemoMode { get; init; }
+    bool? DemoMode { get; }
 
     /// <summary>
     /// Gets a value indicating whether the app should use fewer resources to improve overall performance.
     /// </summary>
-    [OptionalArgument]
-    public bool? PerformanceMode { get; init; }
+    bool? PerformanceMode { get; }
 
     /// <summary>
     /// Gets a value indicating whether the app should skip the introduction process at first-time launch.
     /// </summary>
-    [OptionalArgument]
-    public bool? SkipIntro { get; init; }
+    bool? SkipIntro { get; }
 
     /// <summary>
     /// Gets the verbosity level for logging.
     /// </summary>
-    [OptionalArgument]
-    public uint? Verbosity { get; init; }
+    uint? Verbosity { get; }
 
     /// <summary>
     /// Gets the specified theme to apply at launch.
     /// </summary>
-    [OptionalArgument]
-    public AppTheme? AppTheme { get; init; }
+    AppTheme? AppTheme { get; }
 }
