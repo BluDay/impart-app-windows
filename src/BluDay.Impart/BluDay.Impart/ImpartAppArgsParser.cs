50a95a0a52cc3964a2b96041855248817d2e895d
BluDay <19403893+BluDay@users.noreply.github.com>
2024-07-05 15:50:31 +0200

    refactor: simplify by using new argument descriptor builder classes

--- src/BluDay.Impart/BluDay.Impart/ImpartAppArgsParser.cs
+++ src/BluDay.Impart/BluDay.Impart/ImpartAppArgsParser.cs
@@ -11,73 +11,54 @@ public sealed class ImpartAppArgsParser : ArgumentsParser<ImpartAppArgs>
     public static ImpartAppArgsParser Default { get; } = new();
 
     /// <summary>
-    /// Initializes a new instance with app-specific optional and positional command-line arguments.
+    /// Initializes a new instance of the <see cref="ImpartAppArgs"/> class with app-specific
+    /// optional and positional command-line arguments.
     /// </summary>
-    public ImpartAppArgsParser() : base(CreateArguments()) { }
+    public ImpartAppArgsParser() : base(CreateDescriptors()) { }
 
     /// <summary>
     /// Creates optional and positional command-line argument descriptors.
     /// </summary>
-    /// <returns>An <see cref="ArgumentDescriptors"/> instance of different argument descriptors.</returns>
-    private static ArgumentDescriptors CreateArguments()
+    /// <returns>
+    /// An <see cref="ArgumentDescriptors"/> instance of different argument descriptors.
+    /// </returns>
+    private static ArgumentDescriptors CreateDescriptors()
     {
-        return new(CreateOptionals(), CreatePositionals());
-    }
-
-    /// <summary>
-    /// Creates optional argument descriptors to register.
-    /// </summary>
-    /// <returns>An enumerable of optional argument descriptors.</returns>
-    private static IEnumerable<OptionalArgumentDescriptor> CreateOptionals()
-    {
-        return new List<OptionalArgumentDescriptor>()
-        {
-            new(nameof(ImpartAppArgs.DemoMode))
-            {
-                FlagDescriptor = ArgumentFlagDescriptors.DEMO_MODE,
-                Description    = ArgumentDescriptions.DEMO_MODE
-            },
-            new(nameof(ImpartAppArgs.PerformanceMode))
-            {
-                FlagDescriptor = ArgumentFlagDescriptors.PERFORMANCE_MODE,
-                Description    = ArgumentDescriptions.PERFORMANCE_MODE
-            },
-            new(nameof(ImpartAppArgs.SkipIntro))
-            {
-                FlagDescriptor = ArgumentFlagDescriptors.SKIP_INTRO,
-                Description    = ArgumentDescriptions.SKIP_INTRO
-            },
-            new(nameof(ImpartAppArgs.Verbosity))
-            {
-                FlagDescriptor = ArgumentFlagDescriptors.VERBOSITY,
-                Description    = ArgumentDescriptions.VERBOSITY,
-                ActionKind     = ArgumentActionKind.CountFlag,
-                StoreKind      = ArgumentStoreKind.Integer,
-                Constant       = 1
-            },
-            new(nameof(ImpartAppArgs.AppTheme))
-            {
-                FlagDescriptor = ArgumentFlagDescriptors.APP_THEME,
-                Description    = ArgumentDescriptions.APP_THEME,
-                ActionKind     = ArgumentActionKind.StoreValue,
-                StoreKind      = ArgumentStoreKind.String
-            }
-        };
-    }
-
-    /// <summary>
-    /// Creates positional argument descriptors to register.
-    /// </summary>
-    /// <returns>An enumerable of positional argument descriptors.</returns>
-    private static IEnumerable<PositionalArgumentDescriptor> CreatePositionals()
-    {
-        return new List<PositionalArgumentDescriptor>()
-        {
-            new(nameof(PositionalArgumentDescriptor))
-            {
-                ActionKind = ArgumentActionKind.AppendValue,
-                StoreKind  = ArgumentStoreKind.String
-            }
-        };
+        return new ArgumentDescriptorsBuilder()
+            .Optional()
+                .Name(nameof(ImpartAppArgs.DemoMode))
+                .Description(ArgumentDescriptions.DEMO_MODE)
+                .Flags(ArgumentFlagDescriptors.DEMO_MODE)
+                .Descriptors
+            .Optional()
+                .Name(nameof(ImpartAppArgs.PerformanceMode))
+                .Description(ArgumentDescriptions.PERFORMANCE_MODE)
+                .Flags(ArgumentFlagDescriptors.PERFORMANCE_MODE)
+                .Descriptors
+            .Optional()
+                .Name(nameof(ImpartAppArgs.SkipIntro))
+                .Description(ArgumentDescriptions.SKIP_INTRO)
+                .Flags(ArgumentFlagDescriptors.SKIP_INTRO)
+                .Descriptors
+            .Optional()
+                .Name(nameof(ImpartAppArgs.Verbosity))
+                .Description(ArgumentDescriptions.VERBOSITY)
+                .Flags(ArgumentFlagDescriptors.VERBOSITY)
+                .ActionKind(ArgumentActionKind.CountFlag)
+                .StoreKind(ArgumentStoreKind.Integer)
+                .Constant(1)
+                .Descriptors
+            .Optional()
+                .Name(nameof(ImpartAppArgs.AppTheme))
+                .Description(ArgumentDescriptions.APP_THEME)
+                .Flags(ArgumentFlagDescriptors.APP_THEME)
+                .ActionKind(ArgumentActionKind.StoreValue)
+                .StoreKind(ArgumentStoreKind.String)
+                .Descriptors
+            .Positional()
+                .ActionKind(ArgumentActionKind.AppendValue)
+                .StoreKind(ArgumentStoreKind.String)
+                .Descriptors
+            .Build();
     }
 }
