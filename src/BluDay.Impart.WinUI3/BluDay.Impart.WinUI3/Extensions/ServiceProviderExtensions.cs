a3a406a77713891cce5051e7aab557ced3c1e558
BluDay <19403893+BluDay@users.noreply.github.com>
2024-07-06 01:08:37 +0200

    feat: add new ServiceProviderExtensions class

---
+++ src/BluDay.Impart.WinUI3/BluDay.Impart.WinUI3/Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace BluDay.Impart.WinUI3.Extensions;
+
+/// <summary>
+/// A method extension class for instances of type <see cref="IServiceProvider"/>.
+/// </summary>
+public static class ServiceProviderExtensions
+{
+    /// <summary>
+    /// Launches a WinUI 3 app using the provided DI container.
+    /// </summary>
+    /// <param name="source">
+    /// The root service provider instance.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// If <paramref name="source"/> is null.
+    /// </exception>
+    public static void CreateWinUI3App(this IServiceProvider source)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        WinUI3AppFactory.Create(() => source.GetRequiredService<App>());
+    }
+}
