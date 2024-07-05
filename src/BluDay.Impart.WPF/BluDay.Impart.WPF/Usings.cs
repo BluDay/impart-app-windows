b61635e7480c985ecb14101d953e75a02bd60e3b
BluDay <19403893+BluDay@users.noreply.github.com>
2024-07-05 12:18:03 +0200

    refactor: update namespaces

--- src/BluDay.Impart.WPF/BluDay.Impart.WPF/Usings.cs
+++ src/BluDay.Impart.WPF/BluDay.Impart.WPF/Usings.cs
@@ -1,5 +1,4 @@
-﻿global using CommunityToolkit.Mvvm.ComponentModel;
-global using CommunityToolkit.Mvvm.Messaging;
+﻿global using CommunityToolkit.Mvvm.Messaging;
 
 global using BluDay.Impart;
 global using BluDay.Impart.Domain.ViewModels;
--- src/BluDay.Impart.WinUI3/BluDay.Impart.WinUI3/Usings.cs
+++ src/BluDay.Impart.WinUI3/BluDay.Impart.WinUI3/Usings.cs
@@ -1,6 +1,4 @@
-﻿global using CommunityToolkit.Mvvm.ComponentModel;
-global using CommunityToolkit.Mvvm.DependencyInjection;
-global using CommunityToolkit.Mvvm.Messaging;
+﻿global using CommunityToolkit.Mvvm.Messaging;
 
 global using BluDay.Impart;
 global using BluDay.Impart.Domain.ViewModels;
@@ -9,7 +7,7 @@ global using BluDay.Impart.WinUI3.Controls;
 global using BluDay.Impart.WinUI3.Extensions;
 global using BluDay.Impart.WinUI3.Factories;
 global using BluDay.Impart.WinUI3.Views;
-global using BluDay.Net.Attributes;
+global using BluDay.Net.Common.Attributes;
 global using BluDay.Net.Services;
 global using BluDay.Net.UI.Navigation;
 global using BluDay.Net.UI.Windowing;
@@ -23,7 +21,6 @@ global using Microsoft.UI.Windowing;
 global using Microsoft.UI.Xaml;
 global using Microsoft.UI.Xaml.Controls;
 global using Microsoft.UI.Xaml.Media;
-global using Microsoft.UI.Xaml.Media.Animation;
 
 global using System;
 global using System.Runtime.InteropServices;
