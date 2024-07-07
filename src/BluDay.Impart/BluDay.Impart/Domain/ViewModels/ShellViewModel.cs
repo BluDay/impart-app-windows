acd1207bde99fae9196f5877c4277066af539766
BluDay <19403893+BluDay@users.noreply.github.com>
2024-07-07 23:51:18 +0200

    feat: add new ShellViewModel class

---
+++ src/BluDay.Impart/BluDay.Impart/Domain/ViewModels/ShellViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace BluDay.Impart.Domain.ViewModels;
+
+/// <summary>
+/// View model for an window or shell.
+/// </summary>
+public sealed partial class ShellViewModel : ViewModel
+{
+    /// <summary>
+    /// Gets a value indicating whether the window is resizable.
+    /// </summary>
+    [ObservableProperty]
+    private bool _isResizable;
+
+    /// <summary>
+    /// Gets the title of the window.
+    /// </summary>
+    [ObservableProperty]
+    private string? _title;
+}
