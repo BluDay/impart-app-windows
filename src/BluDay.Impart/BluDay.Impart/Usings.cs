c17d84d8dbc4038ed0f836626437a905eaf2f117
BluDay <19403893+BluDay@users.noreply.github.com>
2024-07-24 21:12:01 +0200

    feat: update global using statements in core assembly

--- src/BluDay.Impart/BluDay.Impart/Usings.cs
+++ src/BluDay.Impart/BluDay.Impart/Usings.cs
@@ -8,18 +8,15 @@ global using BluDay.Impart.ViewModels;
 global using BluDay.Net.Abstractions.Models;
 global using BluDay.Net.Abstractions.UI.Views;
 global using BluDay.Net.Abstractions.ViewModels;
+global using BluDay.Net.Abstractions.UI.Windowing;
 global using BluDay.Net.Common.Attributes;
 global using BluDay.Net.Common.CommandLine;
 global using BluDay.Net.DependencyInjection;
 global using BluDay.Net.Messaging;
 global using BluDay.Net.Services;
 global using BluDay.Net.UI.Appearance;
-global using BluDay.Net.UI.Navigation;
-global using BluDay.Net.UI.Windowing;
 
 global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.DependencyInjection.Extensions;
 global using Microsoft.Extensions.Logging;
 
 global using System.ComponentModel.DataAnnotations;
-global using System.Drawing;
