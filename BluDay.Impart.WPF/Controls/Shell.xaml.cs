6d712bc0acb05fa7e54ec54617118cb16ca3a6c8
BluDay <19403893+BluDay@users.noreply.github.com>
2024-08-07 01:48:27 +0200

    refactor(wpf): rehaul Shell control

---
+++ BluDay.Impart.WPF/Controls/Shell.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace BluDay.Impart.WPF.Controls;
+
+/// <summary>
+/// Interaction logic for Shell.xaml
+/// </summary>
+public sealed partial class Shell : Window, IBluNavigableWindow
+{
+    /// <inheritdoc/>
+    public ViewNavigator ViewNavigator { get; }
+
+    /// <inheritdoc/>
+    public ulong Id => default;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Shell"/> class.
+    /// </summary>
+    public Shell()
+    {
+        // TODO: Inject and store a transient ShellViewModel instance.
+
+        ViewNavigator = null!;
+
+        InitializeComponent();
+    }
+
+    /// <inheritdoc/>
+    public void Resize(int width, int height)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc/>
+    void IBluWindow.Activate() => Activate();
+}
