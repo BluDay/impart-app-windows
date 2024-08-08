8d58d566ca0867043e3467a49c6f1ec029befc78
BluDay <19403893+BluDay@users.noreply.github.com>
2024-08-08 12:44:09 +0200

    feat(winui): use temporary view navigation code in `MainView`

--- BluDay.Impart.WinUI3/Views/MainView.xaml.cs
+++ BluDay.Impart.WinUI3/Views/MainView.xaml.cs
@@ -5,18 +5,42 @@ namespace BluDay.Impart.WinUI3.Views;
 /// </summary>
 public sealed partial class MainView : UserControl
 {
+    private readonly IServiceProvider _serviceProvider;
+
     /// <summary>
     /// Initializes a new instance of the <see cref="MainView"/> class.
     /// </summary>
     /// <param name="viewModel">
     /// A transient <see cref="MainViewModel"/> instance.
     /// </param>
-    public MainView(MainViewModel viewModel, ChatsView chatsView)
+    public MainView(MainViewModel viewModel, IServiceProvider serviceProvider)
     {
+        _serviceProvider = serviceProvider;
+
         DataContext = viewModel;
 
         InitializeComponent();
 
-        ViewContentControl.Content = chatsView;
+        ViewContentControl.Content = serviceProvider.GetRequiredService<ChatsView>();
+    }
+
+    private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+    {
+        if (args.IsSettingsInvoked)
+        {
+            ViewContentControl.Content = _serviceProvider.GetRequiredService<SettingsView>();
+
+            return;
+        }
+
+        string? viewTypeName = ((FrameworkElement)args.InvokedItemContainer)?.Tag as string;
+
+        if (viewTypeName is null) return;
+
+        Type? viewType = Type.GetType(viewTypeName);
+
+        if (viewType is null) return;
+
+        ViewContentControl.Content = _serviceProvider.GetRequiredService(viewType);
     }
 }
