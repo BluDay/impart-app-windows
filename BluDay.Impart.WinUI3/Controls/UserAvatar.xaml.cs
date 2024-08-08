13cadca7cf4ef4e1f2137482e34098750df59628
BluDay <19403893+BluDay@users.noreply.github.com>
2024-08-08 08:45:03 +0200

    feat(winui): add new `UserAvatar` control

---
+++ BluDay.Impart.WinUI3/Controls/UserAvatar.xaml.cs
@@ -0,0 +1,12 @@
+namespace BluDay.Impart.WinUI3.Controls;
+
+/// <summary>
+/// Represents the user avatar or picture control.
+/// </summary>
+public sealed partial class UserAvatar : UserControl
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UserAvatar"/> class.
+    /// </summary>
+    public UserAvatar() => InitializeComponent();
+}
